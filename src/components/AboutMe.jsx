263d46193586d9fdc5285b47ea36cc94733d1376
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-17 14:45:54 -0500

    skills list

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,6 +4,23 @@ const AboutMe = () => {
     return (
         <div className="AboutMe">
             <h1>Taylor Lacerte</h1>
+            <p>I am a full-stack software engineer with a background in tech sales and client success. I have a uniquely positive view of the world, and my decidedly hopeful outlook is what pushes me every day to impact the technology sector in a meaningful way. With my client-facing skills, I am able to build tools that streamline the stakeholder's experience. </p>
+            <h4>Technical Skills</h4>
+            <ul className="skills-list">
+                <li>JavaScript</li>
+                <li>NodeJS</li>
+                <li>Express</li>
+                <li>React</li>
+                <li>MongoDB</li>
+                <li>Python</li>
+                <li>Django</li>
+                <li>PostgreSQL</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>AWS</li>
+                <li>Version Control</li>
+            </ul>
         </div>
     )
 }
