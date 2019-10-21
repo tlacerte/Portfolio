aa134a96a377f16c93504c6908491d1a123b7752
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-21 13:17:58 -0500

    projects folder

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const AboutMe = () => {
     return (
         <div className="AboutMe">
-            <h1>Taylor Lacerte</h1>
+            <h1>About Me</h1>
             <p>I am a full-stack software engineer with a background in tech sales and client success. I have a uniquely positive view of the world, and my decidedly hopeful outlook is what pushes me every day to impact the technology sector in a meaningful way. With my client-facing skills, I am able to build tools that streamline the stakeholder's experience. </p>
             <h4>Technical Skills</h4>
-            <ul className="skills-list">
-                <li>JavaScript</li>
-                <li>NodeJS</li>
-                <li>Express</li>
-                <li>React</li>
-                <li>MongoDB</li>
-                <li>Python</li>
-                <li>Django</li>
-                <li>PostgreSQL</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Git</li>
-                <li>AWS</li>
-                <li>Version Control</li>
-            </ul>
+            <div className="skills-list">
+                <p>JavaScript</p>
+                <p>NodeJS</p>
+                <p>Express</p>
+                <p>React</p>
+                <p>MongoDB</p>
+                <p>Python</p>
+                <p>Django</p>
+                <p>PostgreSQL</p>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>Git</p>
+                <p>AWS</p>
+                <p>Version Control</p>
+            </div>
         </div>
     )
 }
