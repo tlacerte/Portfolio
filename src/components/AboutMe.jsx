0fcbe28e39eb28c3cd3bd0af45ae73f30e2cd920
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-22 13:42:58 -0500

    aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
+import Bonus from './Bonus'
 
 const AboutMe = () => {
     return (
         <div className="AboutMe">
             <h1>About Me</h1>
-            <p>I am a full-stack software engineer with a background in tech sales and client success. I have a uniquely positive view of the world, and my decidedly hopeful outlook is what pushes me every day to impact the technology sector in a meaningful way. With my client-facing skills, I am able to build tools that streamline the stakeholder's experience. </p>
+            <p className="aboutme-p">I am a full-stack software engineer with a background in tech sales and client success. I have a uniquely positive view of the world, and my decidedly hopeful outlook is what pushes me every day to impact the technology sector in a meaningful way. With my client-facing skills, I am able to build tools that streamline the stakeholder's experience. </p>
+            <br />
             <h4>Technical Skills</h4>
             <div className="skills-list">
-                <p>JavaScript</p>
-                <p>NodeJS</p>
-                <p>Express</p>
-                <p>React</p>
-                <p>MongoDB</p>
-                <p>Python</p>
-                <p>Django</p>
-                <p>PostgreSQL</p>
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>Git</p>
-                <p>AWS</p>
-                <p>Version Control</p>
+                <p>JavaScript | NodeJS | Express | React | MongoDb </p>
+                <p>Python | Django | PostgreSQL</p>
+                <p>HTML | CSS | Bootstrap | Git | AWS | Version Control</p>
             </div>
+            <br />
+            <h4>Work Experience</h4>
+            <div className="work-experience">
+                <div className="indeed">
+                    <h5>Client Success Specialist</h5>
+                    <h6>Indeed.com</h6>
+                    <p>Indeed is the #1 job site in the world with over 250 million unique visitors every month. Indeed strives to put job seekers first, giving them free access to search for jobs, post resumes, and research companies. I provided expert support for hundreds of companies every day through navigation of the website, use of the Pay-Per-Click (PPC) advertising model, and support of all Indeed products. I aimed to ensure client satisfaction through collaboration with our Product, Operations, Search Quality and Aggregation teams to troubleshoot problems, and improve processes. </p>
+                </div>
+                <div className="meltwater">
+                    <h5>Software Sales Consultant</h5>
+                    <h6>Meltwater</h6>
+                    <p>Meltwater is the global leader in media intelligence, providing software solutions to over 30,000 organizations around the world. Responsibilities included generating and qualifying leads, maintaining a pipeline of 300 potential clients, and conducting the entire sales process: prospect, cold call, software demonstration, and close.</p>
+                </div>
+            </div>
+        <div>
+            <Bonus />
+        </div>
         </div>
     )
 }
---
+++ src/components/Bonus.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Bonus extends Component {
+    render(){
+        return (
+            <div>
+                <button>Bonus</button>
+            </div>
+        )
+    }
+}
+
+export default Bonus
