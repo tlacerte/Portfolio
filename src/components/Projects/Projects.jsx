85dad7ea282f5f915ec9c3b0010d1153d07105b4
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-25 15:55:27 -0500

    projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,1 +1,11 @@
 import React from 'react'
+
+const Projects = () => {
+    return (
+        <div className="Projects">
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export default Projects
